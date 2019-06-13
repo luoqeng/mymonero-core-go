1f395510a596b4ee7d7c323f73f8bf740b1f5003
luoqeng <luoqeng@gmail.com>
2019-06-13 15:39:35 +0800

    add callback function

--- src/bridge/mymonero.cpp
+++ src/bridge/mymonero.cpp
@@ -48,6 +48,14 @@ string send_cb_III__submitted_tx(const string &args_string)
     }
 }
 */
+string send_step1__prepare_params_for_get_decoys(const string &args_string)
+{
+    try {
+        return serial_bridge::send_step1__prepare_params_for_get_decoys(args_string);
+    } catch (std::exception &e) {
+        return serial_bridge_utils::error_ret_json_from_message(e.what());
+    }
+}
 string send_step2__try_create_transaction(const string &args_string)
 {
     try {
@@ -254,9 +262,34 @@ const char* call_func(const char *fn_name, const char *args_string)
 {
     typedef string (*PTRFUNC)(const string &);
 
+    #define REGISTER_FUNC(fn_name) {#fn_name, &fn_name}
+
     const map<string, PTRFUNC> cbFn {
-        {"generate_key_image", &generate_key_image},
-        {"generate_key_derivation", &generate_key_derivation},
+        REGISTER_FUNC(send_step1__prepare_params_for_get_decoys),
+        REGISTER_FUNC(send_step2__try_create_transaction),
+        REGISTER_FUNC(decode_address),
+        REGISTER_FUNC(is_subaddress),
+        REGISTER_FUNC(is_integrated_address),
+        REGISTER_FUNC(new_integrated_address),
+        REGISTER_FUNC(new_payment_id),
+        REGISTER_FUNC(newly_created_wallet),
+        REGISTER_FUNC(are_equal_mnemonics),
+        REGISTER_FUNC(address_and_keys_from_seed),
+        REGISTER_FUNC(mnemonic_from_seed),
+        REGISTER_FUNC(seed_and_keys_from_mnemonic),
+        REGISTER_FUNC(validate_components_for_login),
+        REGISTER_FUNC(estimated_tx_network_fee),
+        REGISTER_FUNC(estimate_fee),
+        REGISTER_FUNC(estimate_tx_weight),
+        REGISTER_FUNC(estimate_rct_tx_size),
+        REGISTER_FUNC(generate_key_image),
+        REGISTER_FUNC(generate_key_derivation),
+        REGISTER_FUNC(derive_public_key),
+        REGISTER_FUNC(derive_subaddress_public_key),
+        REGISTER_FUNC(derivation_to_scalar),
+        REGISTER_FUNC(decodeRct),
+        REGISTER_FUNC(decodeRctSimple),
+        REGISTER_FUNC(encrypt_payment_id),
     };
 
     string ret_str="{\"err_msg\":\"func not found\"}";
