5b62690b46a5fda4038901cd9f2999f092f7a74e
luoqeng <luoqeng@gmail.com>
2019-07-05 19:07:41 +0800

    fix pkg jsonparser

--- bin/build_monero.sh
+++ bin/build_monero.sh
@@ -1,13 +1,17 @@
 [ -z "${GOPATH}"  ] && GOPATH=${HOME}/go
 
 FIRST_GOPATH=$(echo ${GOPATH}| cut -d':' -f 1)
-PKGPATH=${FIRST_GOPATH}/src/github.com/luoqeng/mymonero-core-go
-[ ! -d "${PKGPATH}"  ] && go get github.com/luoqeng/mymonero-core-go
 
-LIBMONERO=/build/lib/libmymonero.a
-if [ ! -f "${PKGPATH}${LIBMONERO}"  ]
+PKG_JSONPARSE=${FIRST_GOPATH}/src/github.com/buger/jsonparser
+[ ! -d "${PKG_JSONPARSE}"  ] && go get github.com/buger/jsonparser
+
+PKG_MONERO=${FIRST_GOPATH}/src/github.com/luoqeng/mymonero-core-go
+[ ! -d "${PKG_MONERO}"  ] && go get github.com/luoqeng/mymonero-core-go
+
+LIB_MONERO=/build/lib/libmymonero.a
+if [ ! -f "${PKG_MONERO}${LIB_MONERO}"  ]
 then
-    cd ${PKGPATH}
+    cd ${PKG_MONERO}
     ./bin/update_submodules
     [ ! -d build ] && mkdir build
     cd build
